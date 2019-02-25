4696136a3aa89470fd2dcb31c493b6bdc54fc1ab
cz_jjq <cz_jjq@163.com>
2019-02-25 08:22:58 +0800

    upgrade

---
+++ tiny4412/remake.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+make distclean
+#make $1
+export ARCH=arm
+export CROSS_COMPILE=arm-linux-gnueabihf-
+#important:! only origen board support SPL
+make origen_defconfig	
+make V=1 
+arm-linux-gnueabihf-objdump -D u-boot > u-boot.dis
+#rm s5pv210*.bin
+#./mkv210_image  u-boot.bin s5pv210_for_sd.bin
