e7a13384619dfe2b0b0396d37be0a4902fc6e3cf
cz_jjq <cz_jjq@163.com>
2019-03-05 14:33:18 +0800

    upgrade

--- jz2440_v3/arch/arm/lib/.div0.o.cmd
+++
@@ -1,9 +0,0 @@
-cmd_arch/arm/lib/div0.o := arm-linux-gnueabihf-gcc -Wp,-MD,arch/arm/lib/.div0.o.d  -nostdinc -isystem /opt/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf/bin/../lib/gcc/arm-linux-gnueabihf/7.3.1/include -Iinclude   -I./arch/arm/include -include ./include/linux/kconfig.h -D__KERNEL__ -D__UBOOT__ -Wall -Wstrict-prototypes -Wno-format-security -fno-builtin -ffreestanding -std=gnu11 -fshort-wchar -fno-strict-aliasing -fno-PIE -Os -fno-stack-protector -fno-delete-null-pointer-checks -g -fstack-usage -Wno-format-nonliteral -Werror=date-time -D__ARM__ -marm -mno-thumb-interwork -mabi=aapcs-linux -mword-relocations -fno-pic -ffunction-sections -fdata-sections -fno-common -ffixed-r9 -msoft-float -pipe -march=armv4t -D__LINUX_ARM_ARCH__=4    -D"KBUILD_STR(s)=$(pound)s" -D"KBUILD_BASENAME=KBUILD_STR(div0)"  -D"KBUILD_MODNAME=KBUILD_STR(div0)" -c -o arch/arm/lib/div0.o arch/arm/lib/div0.c
-
-source_arch/arm/lib/div0.o := arch/arm/lib/div0.c
-
-deps_arch/arm/lib/div0.o := \
-
-arch/arm/lib/div0.o: $(deps_arch/arm/lib/div0.o)
-
-$(deps_arch/arm/lib/div0.o):
